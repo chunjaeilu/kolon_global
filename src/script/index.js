6cc5cda93e990b6294b781c1e5519cfa48d4b802
KIM MINSU <112890661+chunjaeilu@users.noreply.github.com>
2022-12-08 02:56:15 +0900

    section2 구현, section1 작성중

--- src/script/index.js
+++ src/script/index.js
@@ -1,1 +1,23 @@
 /* index.js */
+$(function () {
+  /* section2 호버 >> bg 전환 */
+  let bgEl = [
+    "src/images/section2-bg1.jpg",
+    "src/images/section2-bg2.jpg",
+    "src/images/section2-bg3.jpg",
+    "src/images/section2-bg4.jpg",
+  ];
+
+  $(".section2 .bl-list > li").hover(function () {
+    let num = $(this).index();
+    $(".section2 .container").css({
+      backgroundImage: `url(${bgEl[num]})`,
+      transition: "1s",
+    });
+  });
+
+  /* footer link-box 클릭 >> 목록 노출 */
+  $("footer .link-box .link-depth1 > li").on("click", function () {
+    $(this).children(".link-depth2").toggleClass("show");
+  });
+});
