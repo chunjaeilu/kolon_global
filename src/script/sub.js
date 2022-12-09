4872885940cbf513078ee7a6b7484f54a04c9747
chunjaeilu <chunjaeilu@naver.com>
2022-12-09 13:11:21 +0900

    서브1-1 모바일 작성

--- src/script/sub.js
+++ src/script/sub.js
@@ -1,5 +1,10 @@
 /* sub.js */
 $(function () {
+  /* mobile GNB 토글기능 */
+  $("#btn-gnb-toggle").on("click", function () {
+    $("header .gnb").toggleClass("show");
+  });
+
   /* footer link-box 클릭 >> 목록 노출 */
   $("footer .link-box .link-depth1 > li").on("click", function () {
     $(this).children(".link-depth2").toggleClass("show");
